b57366ba829af65ad9ba197deffe0068750c420c
sustapakhreen <sushta48@gmail.com>
2023-03-16 17:02:18 -0700

    Completed Control Flow Lab

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+
+function scuberGreetingForFeet(ride){
+  let result;
+  if ( ride <= 400){
+    result = 'This one is on me!'
+  }
+  else if(ride > 400 && ride <= 2000){
+   result = 'That will be twenty bucks.'
+  }
+  else if(ride > 2000 && ride <= 2500){
+    result = 'I will gladly take your thirty bucks.'
+  }
+  else {
+    result = 'No can do.'
+  }
+  return result;
+
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  
+  let message = (city === 'NYC') ? ('Ok, sounds good.') : ('No go.');
+  return message;
 }
 
-function switchOnCharmFromTip(){
+
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let response = '';
+switch (tip) {
+  case 'generous':
+    response = 'Thank you so much.'
+    break;
+
+  case 'not as generous':
+    response = 'Thank you.'
+    break;
+  
+
+  default:
+    response = 'Bye.'
+}
+return response;
 }
